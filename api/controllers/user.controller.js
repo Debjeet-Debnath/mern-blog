2679b1f157e16cf6e09b0758b23127b2337a92d0
debjeesious79 <nathdebjeet79@gmail.com>
2024-12-29 22:50:37 +0530

    Created test API route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,11 @@
+// we use this js file to implement logic for the whole of the program
+
+export const test = (req,res) => {
+    res.json(
+        {
+            message: "API is Working",
+        }
+    )
+}
+
+
